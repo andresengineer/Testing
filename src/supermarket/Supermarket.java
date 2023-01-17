9833bf4da71971cab814747728716cb5ee929071
Andres Zapata <mosquera.wilson@correounivalle.edu.co>
2023-01-17 18:45:59 -0500

    Inicio el MiniProyecto 4 p2

--- src/supermarket/Supermarket.java
+++ src/supermarket/Supermarket.java
@@ -16,6 +16,7 @@ public class Supermarket {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
     
 }
